2b3d116b310422e83691e5464a11a6b37748ca44
lighteko <dev.lighteko@gmail.com>
2026-03-23 22:08:20 +0900

    chore: revise auths table to include is_verified column

--- .sql/ddl.sql
+++ .sql/ddl.sql
@@ -18,6 +18,7 @@ CREATE TABLE IF NOT EXISTS prism_user_auths_l
     provider         VARCHAR(20)  NOT NULL,
     provider_user_id VARCHAR(255) NOT NULL,
     email            VARCHAR(320) NOT NULL,
+    is_verified      BOOLEAN      NOT NULL DEFAULT FALSE,
     password_hash    TEXT,
     created_at       TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
     updated_at       TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
